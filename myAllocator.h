4808bbea116b27c9478e664cf1dbe5df261a2669
ctrlcandy <alexandraleshanu@yandex.ru>
2020-03-31 02:45:27 +0300

    Исправлены недочеты

--- myAllocator.h
+++ myAllocator.h
@@ -9,18 +9,18 @@ class myAllocator {
 
 public:
 
-    explicit myAllocator(std::size_t n) : _size(n) {};
+    myAllocator(std::size_t n) noexcept : _size(n) {};
 
     T* allocate(std::size_t n) const {
         if (n > _size) {
-            throw std::invalid_argument("Wrong value");
+            throw std::bad_alloc();
         }
         else {
            return new T[n];
         }
     }
 
-    void deallocate(T* p, std::size_t n) const {
+    void deallocate(T* p, std::size_t n) const noexcept {
         delete[] p;
     }
 
@@ -33,9 +33,19 @@ public:
         p->~T();
     }
 
+    T max_size() const noexcept {
+        return sizeof(T);
+    }
+
 private:
 
     size_t _size;
 };
+
+template <class T,class U>
+bool operator==(const myAllocator <T>&, const myAllocator <U>&) { return true; }
+
+template <class T,class U>
+bool operator!=(const myAllocator <T>&, const myAllocator <U>&) { return false; }
 
 #endif //UNTITLED_MYALLOCATOR_H
